e7dcfd6349a7799b131ff07a3eb366e9c649870f
SaturnSky <ikadro@gmail.com>
2026-03-30 18:44:45 +0900

    chore: Swift 6.0 언어 모드 활성화 및 Yams 6.x 업그레이드

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["MetaHangulIME"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "6.0.0")
     ],
     targets: [
         .target(
